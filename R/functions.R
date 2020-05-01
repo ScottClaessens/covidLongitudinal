44691ca783dc2eaf80912f72d9326b720fa3ba9f
ScottClaessens <scott.claessens@gmail.com>
2020-05-01 17:52:42 +1200

    update figures for new data

--- R/functions.R
+++ R/functions.R
@@ -1,99 +1,26 @@
-# custom functions
-addCountryFromIP <- function(d) {
-  # use rgeolocate package
-  file <- system.file("extdata","GeoLite2-Country.mmdb", package = "rgeolocate")
-  # create data
-  d <-
-    d %>%
-    mutate(country_name.1   = maxmind(IPAddress.1, file, "country_name")$country_name,
-           country_code.1   = maxmind(IPAddress.1, file, "country_code")$country_code,
-           continent_name.1 = maxmind(IPAddress.1, file, "continent_name")$continent_name,
-           country_name.2   = maxmind(IPAddress.2, file, "country_name")$country_name,
-           country_code.2   = maxmind(IPAddress.2, file, "country_code")$country_code,
-           continent_name.2 = maxmind(IPAddress.2, file, "continent_name")$continent_name,
-           country_name.3   = maxmind(IPAddress.3, file, "country_name")$country_name,
-           country_code.3   = maxmind(IPAddress.3, file, "country_code")$country_code,
-           continent_name.3 = maxmind(IPAddress.3, file, "continent_name")$continent_name,
-           country_name.4   = maxmind(IPAddress.4, file, "country_name")$country_name,
-           country_code.4   = maxmind(IPAddress.4, file, "country_code")$country_code,
-           continent_name.4 = maxmind(IPAddress.4, file, "continent_name")$continent_name) %>%
-    # categorise by (1) UK, (2) US, (3) Rest of Europe, (4) Other
-    mutate(region.1 = ifelse(country_name.1 == "United Kingdom", "United Kingdom",
-                             ifelse(country_name.1 == "United States", "United States",
-                                    ifelse(country_name.1 != "United Kingdom" & continent_name.1 == "Europe", "Europe (excl. UK)",
-                                           ifelse(is.na(country_name.1), NA, "Other")))),
-           region.2 = ifelse(country_name.2 == "United Kingdom", "United Kingdom",
-                             ifelse(country_name.2 == "United States", "United States",
-                                    ifelse(country_name.2 != "United Kingdom" & continent_name.2 == "Europe", "Europe (excl. UK)",
-                                           ifelse(is.na(country_name.2), NA, "Other")))),
-           region.3 = ifelse(country_name.3 == "United Kingdom", "United Kingdom",
-                             ifelse(country_name.3 == "United States", "United States",
-                                    ifelse(country_name.3 != "United Kingdom" & continent_name.3 == "Europe", "Europe (excl. UK)",
-                                           ifelse(is.na(country_name.3), NA, "Other")))),
-           region.4 = ifelse(country_name.4 == "United Kingdom", "United Kingdom",
-                             ifelse(country_name.4 == "United States", "United States",
-                                    ifelse(country_name.4 != "United Kingdom" & continent_name.4 == "Europe", "Europe (excl. UK)",
-                                           ifelse(is.na(country_name.4), NA, "Other")))))
-  return(d)
-}
-
-loadData <- function(file) {
-  out <-
-    read_sav(file) %>%
-    addCountryFromIP()
-  return(out)
-}
-
-makePlot <- function(d, y1, y2, y3, y4, group1, group2, group3, group4, 
-                     subtitle, ylab, ymin, ymax, legendTitle, foldername, filename, type) {
-  # prepare Children.1
-  d$Children.1 <- ifelse(d$Children.1 == 0, "0",
-                         ifelse(d$Children.1 == 1, "1",
-                                ifelse(d$Children.1 == 2, "2",
-                                       ifelse(is.na(d$Children.1), NA, "3+"))))
-  d$Children.1 <- factor(d$Children.1)
-  # prepare SES_ladder.1
-  d$SES_Ladder.1 <- ifelse(d$SES_Ladder.1 %in% c(1,2,3), "Low SES",
-                           ifelse(d$SES_Ladder.1 %in% c(4,5,6), "Medium SES",
-                                  ifelse(d$SES_Ladder.1 %in% c(7,8,9), "High SES", NA)))
-  d$SES_Ladder.1 <- factor(d$SES_Ladder.1, levels = c("Low SES", "Medium SES", "High SES"))
-  # summary function
-  getSummary <- function(data, y, group) {
-    out <- 
-      data %>% 
-      select(all_of(y), all_of(group)) %>%
-      mutate_at(vars(2), as.factor) %>%
-      drop_na(all_of(group)) %>%
-      group_by_at(2) %>%
-      summarise_at(all_of(y), list(mean = mean, se = std.error), na.rm=T) %>%
-      rename(Group = all_of(group))
-    return(out)
-  }
-  out <-
-    bind_rows(
-      getSummary(d, y1, group1), 
-      getSummary(d, y2, group2), 
-      getSummary(d, y3, group3),
-      getSummary(d, y4, group4)
-    ) %>%
-    mutate(Timepoint = rep(1:4, each = n_distinct(Group)))
-  # if group is "region"
-  if (grepl("region", group1)) {
-    out <- out %>% 
-      mutate(Group = factor(Group, levels = c("United States", "United Kingdom",
-                                              "Europe (excl. UK)", "Other")))
-  }
-  # if group is "Sex.1"
-  if (grepl("Sex", group1)) {
-    out <- out %>% 
-      filter(Group != 3) %>%
-      mutate(Group = factor(ifelse(Group == 1, "Male", "Female"), 
-                            levels = c("Male", "Female")))
-  }
+makePlot <- function(d, y, group, subtitle, ylab, ymin, ymax, legendTitle, foldername, filename, type) {
+  # prepare SSES
+  d$SSES <- ifelse(d$SSES >= 1 & d$SSES < 4, "Low SES",
+                   ifelse(d$SSES >= 4 & d$SSES < 7, "Medium SES",
+                          ifelse(d$SSES >= 7 & d$SSES <= 9, "High SES", NA)))
+  d$SSES <- factor(d$SSES, levels = c("High SES", "Medium SES", "Low SES"))
+  # prepare IPregion
+  d$IPregion <- ifelse(d$IPregion == "UK", "United Kingdom",
+                       ifelse(d$IPregion == "Europe", "Europe (excl. UK)", d$IPregion))
+  d$IPregion <- factor(d$IPregion, levels = unique(d$IPregion)[c(3,2,1,4)])
+  # prepare children
+  d$ChildrenDichot <- factor(ifelse(d$ChildrenDichot==0, "None", "At least one"))
+  # prepare sex
+  d$Sex <- factor(ifelse(d$Sex==1, "Male", "Female"))
   # make plots
   if (type == "line") {
-    out <- 
-      ggplot(out, aes(x = Timepoint, y = mean, colour = Group)) +
+    out <-
+      d %>%
+      drop_na((!!sym(y))) %>%
+      group_by(Time, (!!sym(group))) %>%
+      summarise(mean = mean((!!sym(y))),
+                se = sd((!!sym(y))) / sqrt(n())) %>%
+      ggplot(aes(x = Time, y = mean, colour = (!!sym(group)))) +
       geom_line(position = position_dodge(0.1)) +
       geom_errorbar(aes(ymin = mean - se, ymax = mean + se), width = 0, 
                     position = position_dodge(0.1)) +
@@ -103,7 +30,12 @@ makePlot <- function(d, y1, y2, y3, y4, group1, group2, group3, group4,
   }
   if (type == "bar") {
     out <-
-      ggplot(out, aes(x = Timepoint, y = mean - 1, group = Group, fill = Group)) +
+      d %>%
+      drop_na((!!sym(y))) %>%
+      group_by(Time, (!!sym(group))) %>%
+      summarise(mean = mean((!!sym(y))),
+                se = sd((!!sym(y))) / n()) %>%
+      ggplot(aes(x = Time, y = mean - 1, group = (!!sym(group)), fill = (!!sym(group)))) +
       geom_bar(stat = "identity", position = position_dodge(0.6), width = 0.5) +
       geom_errorbar(aes(ymin = mean - se - 1, ymax = mean + se - 1), width = 0, 
                     position = position_dodge(0.6)) +
@@ -143,14 +75,8 @@ makePlotList <- function(d, plotPars, type) {
   out <- list()
   for (i in 1:nrow(plotPars)) {
     out[[i]] <- makePlot(d,
-                         y1  = plotPars$y1[i],
-                         y2  = plotPars$y2[i],
-                         y3  = plotPars$y3[i],
-                         y4  = plotPars$y4[i],
-                         group1 = plotPars$group1[i],
-                         group2 = plotPars$group2[i],
-                         group3 = plotPars$group3[i],
-                         group4 = plotPars$group4[i],
+                         y  = plotPars$y[i],
+                         group = plotPars$group[i],
                          # title = plotPars$title[i],
                          subtitle = plotPars$subtitle[i],
                          ylab  = plotPars$ylab[i],
@@ -197,26 +123,10 @@ makeGridList <- function(plotList, type) {
 
 makeCorPlot <- function(data, timepoint) {
   # data
-  if (timepoint == 1) data <- data %>% select(c(195:194, 206, 200:199, 208:207, 198:196, 205:201, 193:184))
-  if (timepoint == 2) data <- data %>% select(c(356:355, 361:359, 363:362, 358:357, 354:349))
-  if (timepoint == 3) data <- data %>% select(c(374:373, 409, 391, 387, 390, 386, 389, 385, 389, 388, 384))
-  if (timepoint == 4) data <- data %>% select(c(420:419, 450, 437, 433, 436, 432, 435, 431, 434, 430))
-  # sort colnames
-  if (timepoint == 1) colnames(data) <- c("Anxiety", "Stress", "DiseaseAvoidance", "RiskManagement", "RiskPerception",
-                                          "HumanityNBT", "NeighborNBT", "HumanityWillingToHelp",
-                                          "CountryWillingToHelp", "NeighborWillingToHelp", "HumanityCFI",
-                                          "CountryCFI", "NeighborCFI", "AcqCFI", "CloseFriendCFI",
-                                          "HumanityPFISharedFate", "CountryPFISharedFate", "NeighborPFISharedFate",
-                                          "AcqPFISharedFate", "CloseFriendPFISharedFate", "HumanityPFIEmpathy",
-                                          "CountryPFIEmpathy", "NeighborPFIEmpathy", "AcqPFIEmpathy", "CloseFriendPFIEmpathy")
-  if (timepoint == 2) colnames(data) <- c("Anxiety", "Stress", "DiseaseAvoidance", "RiskManagement", "RiskPerception",
-                                          "HumanityNBT", "NeighborNBT", "HumanityWillingToHelp", "NeighborWillingToHelp",
-                                          "HumanityPFISharedFate", "NeighborPFISharedFate", "CloseFriendPFISharedFate", 
-                                          "HumanityPFIEmpathy", "NeighborPFIEmpathy", "CloseFriendPFIEmpathy")
-  if (timepoint %in% c(3, 4)) colnames(data) <- c("Anxiety", "Stress", "RiskManagement",
-                                                  "HumanityNBT", "NeighborNBT", "HumanityWillingToHelp", "NeighborWillingToHelp",
-                                                  "HumanityPFISharedFate", "NeighborPFISharedFate", 
-                                                  "HumanityPFIEmpathy", "NeighborPFIEmpathy")
+  data <-
+    data %>%
+    filter(Time == timepoint) %>%
+    select(c(17:23,27:30,37:38))
   # correlation matrix
   corr <- cor(data, use = "pairwise.complete.obs") %>% round(1)
   # p-values
@@ -231,39 +141,37 @@ makeCorPlot <- function(data, timepoint) {
       pch.col = "grey"
       )
   # save
-  if (timepoint == 1) ggsave(plot + ggtitle("Cohort 1, Timepoint 1"), 
-                             file = "figures/correlations/corC1T1.pdf",
-                             width = 10, height = 10)
-  if (timepoint == 2) ggsave(plot + ggtitle("Cohort 1, Timepoint 2"), 
-                             file = "figures/correlations/corC1T2.pdf",
-                             width = 8, height = 8)
-  if (timepoint == 3) ggsave(plot + ggtitle("Cohort 1, Timepoint 3"), 
-                             file = "figures/correlations/corC1T3.pdf",
-                             width = 7, height = 7)
-  if (timepoint == 4) ggsave(plot + ggtitle("Cohort 1, Timepoint 4"), 
-                             file = "figures/correlations/corC1T4.pdf",
-                             width = 7, height = 7)
+  ggsave(plot + ggtitle(paste0("Cohort 1, Timepoint ", timepoint)), 
+         file = paste0("figures/correlations/corC1T", timepoint, ".pdf"),
+         width = 7,
+         height = 7)
   return(plot)
 }
 
-countTableRegion <- function(data, timepoint) {
-  if (timepoint == 1) out <- data %>% group_by(region.1, country_code.1, country_name.1)
-  if (timepoint == 2) out <- data %>% group_by(region.2, country_code.2, country_name.2)
-  if (timepoint == 3) out <- data %>% group_by(region.3, country_code.3, country_name.3)
-  if (timepoint == 4) out <- data %>% group_by(region.4, country_code.4, country_name.4)
-  out <- out %>% summarise(count = n())
+countTableRegion <- function(data) {
+  data$IPregion <- ifelse(data$IPregion == "UK", "United Kingdom",
+                          ifelse(data$IPregion == "Europe", "Europe (excl. UK)", data$IPregion))
+  data$IPregion <- factor(data$IPregion, levels = unique(data$IPregion)[c(3,2,1,4)])
+  out <- 
+    data %>%
+    filter(Time == 1) %>%
+    group_by(IPregion) %>%
+    summarise(count = n())
   return(out)
 }
 
-worldMapCounts <- function(data, timepoint) {
+worldMapCounts <- function(data) {
+  # use rgeolocate package
+  file <- system.file("extdata","GeoLite2-Country.mmdb", package = "rgeolocate")
+  # create world
   world <- 
     ne_countries(scale = "medium", returnclass = "sf") %>%
     filter(continent %in% unique(continent)[1:6]) %>%
-    mutate(region = ifelse(iso_a2 == "US", "United States",
+    mutate(region = ifelse(iso_a2 %in% c("US", "CA"), "North America",
                            ifelse(iso_a2 == "GB", "United Kingdom",
                                   ifelse(continent == "Europe", 
                                          "Europe (excl. UK)", "Other"))),
-           region = factor(region, levels = c("United States", "United Kingdom",
+           region = factor(region, levels = c("North America", "United Kingdom",
                                                      "Europe (excl. UK)", "Other"))) %>%
     drop_na(region)
   world <- 
@@ -271,27 +179,27 @@ worldMapCounts <- function(data, timepoint) {
     geom_sf(data = world, aes(fill = region, colour = region), colour = NA, alpha = 0.2) + 
     coord_sf(ylim = c(-50, 90), xlim = c(), datum = NA) +
     theme(panel.background = element_rect(fill = 'white'))
-  if (timepoint == 1) data <- data %>% group_by(region.1, country_code.1, country_name.1)
-  if (timepoint == 2) data <- data %>% group_by(region.2, country_code.2, country_name.2)
-  if (timepoint == 3) data <- data %>% group_by(region.3, country_code.3, country_name.3)
-  if (timepoint == 4) data <- data %>% group_by(region.4, country_code.4, country_name.4)
-  data <- data %>% summarise(Count = n())
-  if (timepoint == 1) by <- c("country_code.1" = "iso2")
-  if (timepoint == 2) by <- c("country_code.2" = "iso2")
-  if (timepoint == 3) by <- c("country_code.3" = "iso2")
-  if (timepoint == 4) by <- c("country_code.4" = "iso2")
   data <-
     data %>%
+    mutate(country_code = maxmind(IPAddress, file, "country_code")$country_code,
+           continent_name = maxmind(IPAddress, file, "continent_name")$continent_name,
+           region = factor(ifelse(country_code %in% c("CA", "US"), "North America",
+                                  ifelse(country_code == "GB", "United Kingdom",
+                                         ifelse(continent_name == "Europe", "Europe (excl. UK)",
+                                                "Other")))),
+           region = factor(region, levels = levels(region)[c(2,4,1,3)])) %>%
+    filter(Time == 1) %>%
+    group_by(region, country_code) %>%
+    summarise(Count = n()) %>%
     ungroup() %>%
     # get longitudes and latitudes for country centroids
     left_join(CoordinateCleaner::countryref %>%
                 filter(type == "country") %>%
                 select(iso2, centroid.lon, centroid.lat) %>%
-                distinct(iso2, .keep_all = TRUE), by = by) %>%
+                distinct(iso2, .keep_all = TRUE), 
+              by = c("country_code" = "iso2")) %>%
     rename(region = 1) %>%
-    drop_na(region) %>%
-    mutate(region = factor(region, levels = c("United States", "United Kingdom",
-                                              "Europe (excl. UK)", "Other")))
+    drop_na(region)
   map <- 
     world +
     geom_point(aes(x = centroid.lon, y = centroid.lat, size = Count, 
@@ -304,13 +212,9 @@ worldMapCounts <- function(data, timepoint) {
     theme(axis.title.x = element_blank(),
           axis.title.y = element_blank())
   # save
-  if (timepoint == 1) ggsave(map, file = "figures/worldMapCounts/worldMapCountsC1T1.pdf",
-                             width = 9, height = 4.5)
-  if (timepoint == 2) ggsave(map, file = "figures/worldMapCounts/worldMapCountsC1T2.pdf",
-                             width = 9, height = 4.5)
-  if (timepoint == 3) ggsave(map, file = "figures/worldMapCounts/worldMapCountsC1T3.pdf",
-                             width = 9, height = 4.5)
-  if (timepoint == 4) ggsave(map, file = "figures/worldMapCounts/worldMapCountsC1T4.pdf",
-                             width = 9, height = 4.5)
+  ggsave(map, 
+         file = "figures/worldMapCounts/worldMapCounts.pdf",
+         width = 9, 
+         height = 4.5)
   return(map)
 }
--- R/plan.R
+++ R/plan.R
@@ -1,38 +1,33 @@
 # drake plan
 plan <- drake_plan(
   # load data
-  d = loadData("data/Prolific cohort 1 Timepoint 1 - 4 cleaned.sav"),
+  dLong = read_sav("data/Corona - cohort 1 - Times 1-4 - Long - Excluding missing region.sav"),
+  dWide = read_sav("data/Corona - cohort 1 - Times 1-4 - Wide - Excluding missing region.sav"),
   # line plots
   linePlotList = makePlotList(
-    d, read.csv("data/plotting/lineBarPlots.csv",
-                stringsAsFactors = FALSE,
-                allowEscapes = TRUE), "line"
+    dLong, read.csv("data/plotting/lineBarPlots.csv",
+                    stringsAsFactors = FALSE,
+                    allowEscapes = TRUE), "line"
     ),
   # bar plots
   barPlotList = makePlotList(
-    d, read.csv("data/plotting/lineBarPlots.csv",
-                stringsAsFactors = FALSE,
-                allowEscapes = TRUE), "bar"
+    dLong, read.csv("data/plotting/lineBarPlots.csv",
+                    stringsAsFactors = FALSE,
+                    allowEscapes = TRUE), "bar"
     ),
   # line plot grids
   linePlotGridList = makeGridList(linePlotList, "line"),
   # bar plot grids
   barPlotGridList = makeGridList(barPlotList, "bar"),
-  # # cor plots
-  corC1T1 = makeCorPlot(d, timepoint = 1),
-  corC1T2 = makeCorPlot(d, timepoint = 2),
-  corC1T3 = makeCorPlot(d, timepoint = 3),
-  corC1T4 = makeCorPlot(d, timepoint = 4),
-  # # table
-  tableC1T1 = countTableRegion(d, timepoint = 1),
-  tableC1T2 = countTableRegion(d, timepoint = 2),
-  tableC1T3 = countTableRegion(d, timepoint = 3),
-  tableC1T4 = countTableRegion(d, timepoint = 4),
-  # # world
-  worldC1T1 = worldMapCounts(d, timepoint = 1),
-  worldC1T2 = worldMapCounts(d, timepoint = 2),
-  worldC1T3 = worldMapCounts(d, timepoint = 3),
-  worldC1T4 = worldMapCounts(d, timepoint = 4),
+  # cor plots
+  corC1T1 = makeCorPlot(dLong, timepoint = 1),
+  corC1T2 = makeCorPlot(dLong, timepoint = 2),
+  corC1T3 = makeCorPlot(dLong, timepoint = 3),
+  corC1T4 = makeCorPlot(dLong, timepoint = 4),
+  # table
+  tableCount = countTableRegion(dLong),
+  # world
+  worldMap = worldMapCounts(dLong),
   # render report
   report = rmarkdown::render(
     knitr_in("report.Rmd"),
